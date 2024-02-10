4bd74d4613d6669c7a847372e43856b170511593
IFEE09 <ivanfaridespadas@gmail.com>
2024-02-10 00:43:11 -0600

    agregando button en vez de input

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,9 +1,14 @@
-var numero1 = document.getElementById('numero1');
-var numero2 = document.getElementById('numero2');
 
 function sumar() {
-    
-    var resultado = numero1 + numero2;
+    let numero1 = document.getElementById('numero1').value;
+    let numero2 = document.getElementById('numero2').value;
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+    console.log(numero1);
+    console.log(numero2);
+    let resultado = numero1 + numero2;
     alert(resultado); 
-
 }
+
+numero1.value = numero1;
+numero2.value = numero2;
