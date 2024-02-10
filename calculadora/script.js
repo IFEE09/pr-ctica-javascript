74ccd921a81ae658bcc660123e772ed06ffd0752
IFEE09 <ivanfaridespadas@gmail.com>
2024-02-10 00:50:04 -0600

    finalizando calculadora simple con javascript

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,14 +1,15 @@
 
 function sumar() {
+
+    //proceso para sumar
     let numero1 = document.getElementById('numero1').value;
     let numero2 = document.getElementById('numero2').value;
+    //aqui convertimos el valor en numeros enteros con la funcion parseint
     numero1 = parseInt(numero1);
     numero2 = parseInt(numero2);
     console.log(numero1);
     console.log(numero2);
     let resultado = numero1 + numero2;
-    alert(resultado); 
-}
 
-numero1.value = numero1;
-numero2.value = numero2;
+    document.getElementById('resultado').value = resultado;
+}
